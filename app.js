8b61bafabb5b59783919ed567e59f3ff3155f24f
Yazid Ismail <flight846@gmail.com>
2016-06-03 23:40:22 +0800

    Init repo

---
+++ app.js
@@ -0,0 +1,44 @@
+//console.clear();
+console.log("Loaded");
+
+var correctAnswer = [];
+
+
+function numberOfQuestions() {
+
+}
+
+
+function currentQuestion() {
+
+}
+
+
+function correctAnswer() {
+
+}
+
+
+function numberOfAnswers() {
+
+}
+
+
+function playTurn(choice) {
+
+}
+
+
+function isGameOver() {
+
+}
+
+
+function whoWon() {
+
+}
+
+
+function restart() {
+
+}
